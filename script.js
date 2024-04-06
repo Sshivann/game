6e13c5710ea075f81c1b7900477440eed9800f28
sshivann <shivanthapa01@gmail.com>
2024-04-06 16:49:08 +0530

    done with functions

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 function playRound(playerSelection, computerSelection) {
-  // your code here!
+  if (!isValidChoice(playerSelection)) {
+    return "Invaild choice. Please enter rock, paper, or scissors";
+  }
+  if (playerSelection === computerSelection) {
+    return "It is a tie!";
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "rock")
+  ) {
+    return "you win!";
+  } else {
+    return "Computer wins!";
+  }
+}
+function isValidChoice(choice) {
+    return choice === 'rock' || choice === 'paper' || choice === 'scissors'
+}
+function  getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomNumber = Math,floor(Math,random()*3);
+    return choices[randomNumber];
 }
 
 const playerSelection = "rock";
